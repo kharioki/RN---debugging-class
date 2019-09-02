34e33ceeebd2bb66a4cf000570a0088d4d49fddf
kharioki <tony.kharioki@gmail.com>
2019-09-02 09:55:03 +0300

    config

---
+++ App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Button, View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+});
+
+class App extends React.Component {
+  state = { number: null };
+
+  componentDidMount() {
+    this.getNumber();
+  }
+
+  getNumber = () => {
+    fetch('https://debugging-class-server.glitch.me/number')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          number: res.number
+        });
+      })
+      .catch(err => {
+        alert('an error occurred!');
+      });
+  };
+
+  incrementNumber = () => {
+    this.setState({
+      number: this.state.number + 1
+    });
+  };
+
+  decrementNumber = () => {
+    this.setState({
+      number: this.state.number - 1
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>{this.state.number}</Text>
+        <Button title="Increment Number" onPress={this.incrementNumber} />
+        <Button title="Decrement Number" onPress={this.decrementNumber} />
+        <Button title="Get New Number" onPress={this.getNumber} />
+      </View>
+    );
+  }
+}
+
+export default App;
